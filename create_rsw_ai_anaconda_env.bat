2bc65c623a765a762820957f673859a1e4c3b75e
name_lss01 <you@example.com>
2026-06-21 18:45:35 +0800

    123

---
+++ create_rsw_ai_anaconda_env.bat
@@ -0,0 +1,61 @@
+
+
+@echo off
+
+set conda_env_name=rsw_ai
+set conda_env_config_file_name=rsw_ai_anaconda_env.yml
+
+echo ==================================
+echo Creating Conda Environment...
+echo ==================================
+
+
+call conda env list | findstr "%conda_env_name%" >nul
+
+if %errorlevel%==0 (
+    echo Environment %conda_env_name% already exists.
+) else (
+    echo Creating environment...
+    call conda env create -f %conda_env_config_file_name%
+
+    if errorlevel 1 (
+        echo.
+        echo Environment creation failed.
+        pause
+        exit /b 1
+    )
+)
+
+echo.
+echo ==================================
+echo Activating Environment...
+echo ==================================
+
+call conda activate %conda_env_name%
+
+if errorlevel 1 (
+    echo Failed to activate environment.
+    pause
+    exit /b 1
+)
+
+echo.
+echo ==================================
+echo Registering Jupyter Kernel...
+echo ==================================
+
+python -m ipykernel install --user ^
+    --name %conda_env_name% ^
+    --display-name "Python (%conda_env_name%)"
+    
+
+echo.
+echo ==================================
+echo Finished
+echo ==================================
+
+python --version
+
+conda env list
+
+pause
